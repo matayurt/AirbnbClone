7bb32a3102187f9f0f1fc97b7c20f9f2546e43b9
Mehmet <matayurt01@gmail.com>
2024-07-11 00:07:05 +0300

    RegisterForm

---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from 'axios'
+
+const RegisterForm = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const registerHandler = async (e) => {
+        e.preventDefault()
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+            const { data } = await axios.post('/api/users/register', { name, email, password }, config);
+            console.log(data);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={registerHandler}>
+                <div>
+                    <label>Name</label>
+                    <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </>
+    )
+
+}
