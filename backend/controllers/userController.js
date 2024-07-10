b1d17e21ff7cfdc4e21b6e1517855148971b5c63
Mehmet <matayurt01@gmail.com>
2024-07-10 23:22:57 +0300

    UserModel-UserController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,67 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel";
+import bcrypt from "bcryptjs"; // Şifreleri hashlemek güvenli bir şekilde kullanmak için
+import jwt from "jsonwebtoken";
+
+// Kullanıcı Kayıt
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
+});
+
+// Kullanıcı Giriş
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password!");
+  }
+});
+
+// JWT Token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+export default { registerUser, authUser };
