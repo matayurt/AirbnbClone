0d5bb91169711642a04b64a9e2bede4f3deae0b9
Mehmet <matayurt01@gmail.com>
2024-07-10 23:31:27 +0300

    server update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ConnectDB from "./config/db";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5001;
 //Midlewares
 app.use(cors());
 app.use(express.json());
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on port: ", PORT);
