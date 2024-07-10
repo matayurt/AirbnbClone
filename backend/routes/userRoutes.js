6d6bc2a76f6bfcafdf140c198693c7ac6a9f5c9e
Mehmet <matayurt01@gmail.com>
2024-07-10 23:26:44 +0300

    userRoutes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { registerUser, authUser } from "../controllers/userController.js";
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", authUser);
+
+export default router;
