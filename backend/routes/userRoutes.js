839695b69d1329dd9406c2b098051e2a138c47ef
Mehmet <matayurt01@gmail.com>
2024-07-12 18:47:52 +0300

    Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { registerUser, authUser } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", authUser);
+router.get("/profile", protect, (req, res) => {
+  res.json(req.user);
+});
 
 export default router;
