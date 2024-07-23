3a078cae6087f4f1e4809a7c98e27113994cd5a8
Mehmet <matayurt01@gmail.com>
2024-07-23 17:26:32 +0300

    ResetPassword-Style

--- frontend/src/components/ResetPasswordForm.jsx
+++ frontend/src/components/ResetPasswordForm.jsx
@@ -10,33 +10,39 @@ const ResetPasswordForm = () => {
 
     const submitHandler = async (e) => {
         e.preventDefault();
-        try {   
-            const { data } = await axios.post(`http://localhost:5001/api/users/reset-password`, {token, newPassword})
+        try {
+            const { data } = await axios.post(`http://localhost:5001/api/users/reset-password`, { token, newPassword });
             setMessage(data.message);
             setError('');
         } catch (error) {
             setError(error.response.data.message);
             setMessage('');
-            
         }
     }
 
-    return(
-        <>
-            <form onSubmit={submitHandler}>
-                <div>
-                    <label>New Password</label>
-                    <input 
-                        type="password"
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                     />
-                </div>
-                <button type='submit'>Reset Password</button>
-                {message && <div>{message}</div>}
-                {error && <div>{error}</div>}
-            </form>
-        </>
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-white shadow-lg">
+                <h1 className="text-2xl font-bold text-center">Reset Password</h1>
+                <form onSubmit={submitHandler} className="space-y-6">
+                    <div>
+                        <label className="block text-sm">New Password</label>
+                        <input
+                            type="password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            required
+                            className="w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-600"
+                        />
+                    </div>
+                    <div>
+                        <button type="submit" className="btn btn-primary w-full">Reset Password</button>
+                    </div>
+                    {message && <div className="text-green-500 text-center">{message}</div>}
+                    {error && <div className="text-red-500 text-center">{error}</div>}
+                </form>
+            </div>
+        </div>
     );
 };
 
--- frontend/src/components/ResetPasswordRequestForm.jsx
+++ frontend/src/components/ResetPasswordRequestForm.jsx
@@ -6,35 +6,42 @@ const ResetPasswordRequestForm = () => {
     const [message, setMessage] = useState('');
     const [error, setError] = useState('');
 
-    const submitHandler = async(e) => {
+    const submitHandler = async (e) => {
         e.preventDefault();
         try {
-            const { data } = await axios.post(`http://localhost:5001/api/users/reset-password-request`, {email});
+            const { data } = await axios.post(`http://localhost:5001/api/users/reset-password-request`, { email });
             setMessage(data.message);
-            setError('')
+            setError('');
         } catch (error) {
             setError(error.response.data.message);
-            setMessage('')
+            setMessage('');
         }
     }
 
-    return(
-        <>
-            <form onSubmit={submitHandler}>
-                <div>
-                    <label>Email</label>
-                    <input 
-                        type="email" 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Send Reset Link</button>
-                {message && <div>{message}</div>}
-                {error && <div>{error}</div>}
-            </form>
-        </>
-    )
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-white shadow-lg">
+                <h1 className="text-2xl font-bold text-center">Reset Password</h1>
+                <form onSubmit={submitHandler} className="space-y-6">
+                    <div>
+                        <label className="block text-sm">Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                            className="w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-600"
+                        />
+                    </div>
+                    <div>
+                        <button type="submit" className="btn btn-primary w-full">Send Reset Link</button>
+                    </div>
+                    {message && <div className="text-green-500 text-center">{message}</div>}
+                    {error && <div className="text-red-500 text-center">{error}</div>}
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default ResetPasswordRequestForm;
